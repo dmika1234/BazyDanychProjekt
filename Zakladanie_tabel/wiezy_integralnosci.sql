28322d8180f6d70680ba0f452305ba8241fea963
dmika1234 <72747159+dmika1234@users.noreply.github.com>
2021-01-15 20:10:45 +0100

    Update wiezy_integralnosci.sql

--- Zakladanie_tabel/wiezy_integralnosci.sql
+++ Zakladanie_tabel/wiezy_integralnosci.sql
@@ -1,4 +1,4 @@
-
+--Klucze główne--
 ALTER TABLE konta ADD PRIMARY KEY (id_konta);
 ALTER TABLE plany ADD PRIMARY KEY (id_planu);
 ALTER TABLE uzytkownicy ADD PRIMARY KEY (id_uzytkownika);
@@ -9,9 +9,11 @@ ALTER TABLE odtworzenia ADD PRIMARY KEY (id_odtworzenia);
 ALTER TABLE odcinki ADD PRIMARY KEY (id_odcinka);
 ALTER TABLE oceny ADD PRIMARY KEY (id_produkcji,id_uzytkownika);
 ALTER TABLE komentarze ADD PRIMARY KEY (id_komentarza);
-
 ALTER TABLE w_kategorii ADD PRIMARY KEY(id_kategorii, id_produkcji);
+----
+--Klucze obce--
 ALTER TABLE konta ADD FOREIGN KEY (id_planu) REFERENCES plany(id_planu) ON UPDATE CASCADE ON DELETE RESTRICT;
+
 ALTER TABLE uzytkownicy ADD FOREIGN KEY (id_konta) REFERENCES konta(id_konta) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 ALTER TABLE platnosci ADD FOREIGN KEY (id_konta)  REFERENCES konta(id_konta);
@@ -36,3 +38,4 @@ ALTER TABLE oceny ADD CHECK (ocena BETWEEN 1 AND 10);
 ALTER TABLE komentarze ADD FOREIGN KEY (id_pop_kom) REFERENCES komentarze(id_komentarza) ON UPDATE CASCADE ON DELETE RESTRICT;
 ALTER TABLE komentarze ADD FOREIGN KEY (id_uzytkownika) REFERENCES uzytkownicy(id_uzytkownika) ON UPDATE CASCADE ON DELETE RESTRICT;
 ALTER TABLE komentarze ADD FOREIGN KEY (id_produkcji) REFERENCES produkcje(id_produkcji) ON UPDATE CASCADE ON DELETE RESTRICT;
+----
