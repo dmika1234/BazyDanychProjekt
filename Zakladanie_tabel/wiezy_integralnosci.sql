7906ba18e1b845cf3ed41b4adfe71ad7574e0a21
tamarafraczek <77505779+tamarafraczek@users.noreply.github.com>
2021-01-16 09:53:12 +0100

    Update wiezy_integralnosci.sql

--- Zakladanie_tabel/wiezy_integralnosci.sql
+++ Zakladanie_tabel/wiezy_integralnosci.sql
@@ -14,23 +14,23 @@ ALTER TABLE w_kategorii ADD PRIMARY KEY(id_kategorii, id_produkcji);
 --Klucze obce--
 ALTER TABLE konta ADD FOREIGN KEY (id_planu) REFERENCES plany(id_planu) ON UPDATE CASCADE ON DELETE RESTRICT;
 
-ALTER TABLE uzytkownicy ADD FOREIGN KEY (id_konta) REFERENCES konta(id_konta) ON UPDATE CASCADE ON DELETE RESTRICT;
+ALTER TABLE uzytkownicy ADD FOREIGN KEY (id_konta) REFERENCES konta(id_konta) ON UPDATE CASCADE ON DELETE SET NULL;
 
-ALTER TABLE platnosci ADD FOREIGN KEY (id_konta)  REFERENCES konta(id_konta);
+ALTER TABLE platnosci ADD FOREIGN KEY (id_konta)  REFERENCES konta(id_konta) ON UPDATE CASCADE ON DELETE SET NULL;
 
 ALTER TABLE produkcje ADD CHECK (rok_produkcji BETWEEN 1800 AND 2100);
 
-ALTER TABLE odcinki ADD FOREIGN KEY (id_produkcji) REFERENCES produkcje(id_produkcji) ON UPDATE CASCADE ON DELETE RESTRICT;
+ALTER TABLE odcinki ADD FOREIGN KEY (id_produkcji) REFERENCES produkcje(id_produkcji) ON UPDATE CASCADE ON DELETE CASCADE;
 
 ALTER TABLE odtworzenia ADD FOREIGN KEY (id_uzytkownika) REFERENCES uzytkownicy(id_uzytkownika) ON UPDATE CASCADE ON DELETE RESTRICT;
-ALTER TABLE odtworzenia ADD FOREIGN KEY (id_produkcji) REFERENCES produkcje(id_produkcji) ON UPDATE CASCADE ON DELETE RESTRICT;
+ALTER TABLE odtworzenia ADD FOREIGN KEY (id_produkcji) REFERENCES produkcje(id_produkcji) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE odtworzenia ADD FOREIGN KEY (id_odcinka) REFERENCES odcinki(id_odcinka) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 ALTER TABLE w_kategorii ADD FOREIGN KEY (id_kategorii) REFERENCES kategorie(id_kategorii) ON UPDATE CASCADE ON DELETE RESTRICT;
-ALTER TABLE w_kategorii ADD FOREIGN KEY (id_produkcji) REFERENCES produkcje(id_produkcji) ON UPDATE CASCADE ON DELETE RESTRICT;
+ALTER TABLE w_kategorii ADD FOREIGN KEY (id_produkcji) REFERENCES produkcje(id_produkcji) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
-ALTER TABLE oceny ADD FOREIGN KEY (id_produkcji) REFERENCES produkcje(id_produkcji) ON UPDATE CASCADE ON DELETE RESTRICT;
+ALTER TABLE oceny ADD FOREIGN KEY (id_produkcji) REFERENCES produkcje(id_produkcji) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE oceny ADD FOREIGN KEY (id_uzytkownika) REFERENCES uzytkownicy(id_uzytkownika) ON UPDATE CASCADE ON DELETE RESTRICT;
 ALTER TABLE oceny ADD CHECK (ocena BETWEEN 1 AND 10);
 
