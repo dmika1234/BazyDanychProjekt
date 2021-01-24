1b72e3ce652965edf9e182871cf78c9c2b84e2e0
Dominik <dmika1234@gmail.com>
2021-01-24 17:22:55 +0100

    Views

--- Funkcje,triggery,widoki/widoki.sql
+++ Funkcje,triggery,widoki/widoki.sql
@@ -1,20 +1,35 @@
 --Top produkcji--
 DROP VIEW top_produkcji;
 CREATE VIEW top_produkcji AS
-SELECT p.tytul, round(avg(ocena), 2) AS "Srednia ocen"
+SELECT p.tytul AS "Tytul", round(avg(ocena), 2) AS "Srednia ocen"
 FROM oceny o
-JOIN produkcje p ON o.id_produkcji = p.id_produkcji
+	JOIN produkcje p ON o.id_produkcji = p.id_produkcji
 GROUP BY o.id_produkcji, p.tytul
-HAVING COUNT(o.ocena)>10
+	HAVING COUNT(o.ocena)>10
 ORDER BY avg(o.ocena) DESC
 LIMIT 10;
 --
 
 
 
---wgląd w produkcje--
+--wgląd w filmy--
+DROP VIEW info_filmy;
+CREATE VIEW info_filmy AS
+SELECT p.tytul AS "Tytul", p.rezyser AS Rezyser, p.kraj AS kraj, p.dlugosc_filmu AS Dlugosc, p.rok_produkcji AS "Rok produkcji", k.nazwa_kategorii AS Kategoria
+FROM produkcje p
+	JOIN w_kategorii w ON p.id_produkcji = w.id_produkcji
+		JOIN  kategorie k ON w.id_kategorii = k.id_kategorii
+WHERE czy_serial = FALSE;
+--
 
-
-
-
+--wgląd w odcinki--
+DROP VIEW info_odcinki;
+CREATE VIEW info_odcinki AS 
+SELECT p.tytul AS "Tytul serialu", o.tytul_odcinka AS "Tytul odcinka", o.nr_sezonu AS "Numer sezonu", o.nr_odcinka AS "Numer odcinka",
+ p.rezyser AS Rezyser, p.kraj AS Kraj, o.dlugosc_odcinka AS "Dlugosc odcinka", p.rok_produkcji AS "Rok produkcji", k.nazwa_kategorii AS Kategoria
+FROM produkcje p
+	JOIN w_kategorii w ON p.id_produkcji = w.id_produkcji
+		JOIN  kategorie k ON w.id_kategorii = k.id_kategorii
+			JOIN odcinki o ON p.id_produkcji = o.id_produkcji
+WHERE czy_serial = TRUE;
 --
