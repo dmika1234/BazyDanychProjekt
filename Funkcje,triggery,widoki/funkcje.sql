c94bd1d288a1e154562f41927d954c4e4e434eb6
tamarafraczek <77505779+tamarafraczek@users.noreply.github.com>
2021-01-24 20:17:27 +0100

    Update funkcje.sql

--- Funkcje,triggery,widoki/funkcje.sql
+++ Funkcje,triggery,widoki/funkcje.sql
@@ -98,3 +98,29 @@ BEGIN
 
 END;
 $$ LANGUAGE plpgsql;
+							   
+							   
+							   
+							   
+
+							   
+							   
+--funkcje topu				
+							   
+CREATE OR REPLACE FUNCTION top(id_kat INTEGER) RETURNS TABLE(tytul VARCHAR(255), srednia DECIMAL(5, 2)) AS $$
+BEGIN
+	RETURN QUERY 
+		SELECT p.tytul, round(avg(o.ocena), 2)
+		FROM produkcje p
+			JOIN oceny o ON p.id_produkcji = o.id_produkcji
+			JOIN w_kategorii w ON w.id_produkcji = p.id_produkcji
+		WHERE w.id_kategorii = id_kat
+		GROUP BY p.tytul
+			HAVING COUNT(o.ocena)>10
+		ORDER BY avg(o.ocena) DESC
+		LIMIT 10;
+
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM top(2);
