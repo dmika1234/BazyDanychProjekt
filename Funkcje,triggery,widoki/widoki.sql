7675294af51f735edb7b591a169817c5a23cdac0
Dominik <dmika1234@gmail.com>
2021-01-24 20:43:04 +0100

    Lot

--- Funkcje,triggery,widoki/widoki.sql
+++ Funkcje,triggery,widoki/widoki.sql
@@ -18,7 +18,7 @@ CREATE VIEW info_filmy AS
 SELECT p.tytul AS "Tytul", p.rezyser AS Rezyser, p.kraj AS kraj, p.dlugosc_filmu AS Dlugosc, p.rok_produkcji AS "Rok produkcji", k.nazwa_kategorii AS Kategoria
 FROM produkcje p
 	JOIN w_kategorii w ON p.id_produkcji = w.id_produkcji
-		JOIN  kategorie k ON w.id_kategorii = k.id_kategorii
+	JOIN  kategorie k ON w.id_kategorii = k.id_kategorii
 WHERE czy_serial = FALSE;
 --
 
@@ -29,7 +29,21 @@ SELECT p.tytul AS "Tytul serialu", o.tytul_odcinka AS "Tytul odcinka", o.nr_sezo
  p.rezyser AS Rezyser, p.kraj AS Kraj, o.dlugosc_odcinka AS "Dlugosc odcinka", p.rok_produkcji AS "Rok produkcji", k.nazwa_kategorii AS Kategoria
 FROM produkcje p
 	JOIN w_kategorii w ON p.id_produkcji = w.id_produkcji
-		JOIN  kategorie k ON w.id_kategorii = k.id_kategorii
-			JOIN odcinki o ON p.id_produkcji = o.id_produkcji
+	JOIN  kategorie k ON w.id_kategorii = k.id_kategorii
+	JOIN odcinki o ON p.id_produkcji = o.id_produkcji
 WHERE czy_serial = TRUE;
 --
+
+
+--komentarze produkcji--??
+DROP VIEW kom_prod;
+CREATE VIEW kom_prod AS
+SELECT p.tytul AS "Tytul produkcji", o.tytul_odcinka AS "Tytul odcinka", k.tresc "Tresc komentarza"
+FROM komentarze k
+	JOIN produkcje p ON k.id_produkcji = p.id_produkcji
+	JOIN odcinki o ON k.id_odcinka = o.id_odcinka;
+----
+
+
+
+
