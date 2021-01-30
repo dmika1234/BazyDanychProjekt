16b09388cdced682d0b484bbf1d5eda2826b3141
tamarafraczek <77505779+tamarafraczek@users.noreply.github.com>
2021-01-30 23:22:09 +0100

    Update funkcje.sql

--- Funkcje,triggery,widoki/funkcje.sql
+++ Funkcje,triggery,widoki/funkcje.sql
@@ -126,6 +126,56 @@ END;
 $$ LANGUAGE plpgsql;
 
 SELECT * FROM top(2);
+												 
+												 
+												 
+												 
+												 
+CREATE OR REPLACE FUNCTION top_f(id_kat INTEGER) RETURNS TABLE(tytul VARCHAR(255), srednia DECIMAL(5, 2)) AS $$
+BEGIN
+	RETURN QUERY 
+		SELECT p.tytul, round(avg(o.ocena), 2)
+		FROM produkcje p
+			JOIN oceny o ON p.id_produkcji = o.id_produkcji
+			JOIN w_kategorii w ON w.id_produkcji = p.id_produkcji
+		WHERE w.id_kategorii = id_kat AND p.czy_serial = FALSE
+		GROUP BY p.tytul
+			HAVING COUNT(o.ocena)>10
+		ORDER BY avg(o.ocena) DESC;
+		
+
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM top(2);
+			
+												   
+												   
+												   
+												   
+												   
+												   
+												   
+												   
+CREATE OR REPLACE FUNCTION top_s(id_kat INTEGER) RETURNS TABLE(tytul VARCHAR(255), srednia DECIMAL(5, 2)) AS $$
+BEGIN
+	RETURN QUERY 
+		SELECT p.tytul, round(avg(o.ocena), 2)
+		FROM produkcje p
+			JOIN oceny o ON p.id_produkcji = o.id_produkcji
+			JOIN w_kategorii w ON w.id_produkcji = p.id_produkcji
+		WHERE w.id_kategorii = id_kat AND p.czy_serial = TRUE
+		GROUP BY p.tytul
+			HAVING COUNT(o.ocena)>10
+		ORDER BY avg(o.ocena) DESC;
+		
+
+END;
+$$ LANGUAGE plpgsql;
+												 
+												 
+												 
+												
 
 
 
